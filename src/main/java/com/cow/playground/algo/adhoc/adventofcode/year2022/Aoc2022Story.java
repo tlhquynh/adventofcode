91a96795e84167867fa4ae648f0421e48702bfa3
QT <cowads@gmail.com>
2022-12-02 21:49:39 +1100

    Add solution for AOC 2022 Day 1

---
+++ src/main/java/com/cow/playground/algo/adhoc/adventofcode/year2022/Aoc2022Story.java
@@ -0,0 +1,11 @@
+package com.cow.playground.algo.adhoc.adventofcode.year2022;
+
+import java.io.IOException;
+
+public class Aoc2022Story {
+    public static void main(String[] args) throws IOException {
+        // Day 1 - Calorie counting
+        CalorieCounting cc = new CalorieCounting();
+        cc.findTops();
+    }
+}
