16ca34e8d25243d151898464a1646567e258e792
Quynh Tran <1598546+tlhquynh@users.noreply.github.com>
2022-12-13 00:31:14 +1100

    Update day 12 task 2 to go forward like task 1 instead of backward/reverse

--- src/main/java/com/cow/playground/algo/adhoc/adventofcode/year2022/HillClimbing.java
+++ src/main/java/com/cow/playground/algo/adhoc/adventofcode/year2022/HillClimbing.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidParameterException;
 import java.util.*;
 
 public class HillClimbing {
@@ -17,9 +16,11 @@ public class HillClimbing {
     private static final String INPUT_PATH = "year2022\\" + INPUT_FILENAME;
     List<char[]> map;
     Location start, end;
+
     public HillClimbing() throws IOException {
         readInputs();
     }
+
     private void readInputs() throws IOException {
 
         try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(INPUT_PATH);
@@ -45,15 +46,39 @@ public class HillClimbing {
             }
         }
     }
+
     public void task1() {
+        int res = floodfill(Collections.<Location>singletonList(start), end);
+        assert res == 449;
+        LOGGER.info("S -> E in {} steps", res);
+    }
+
+    public void task2() {
+        List<Location> starts = new ArrayList<>();
+        for (int i = 0; i < map.size(); i++) {
+            char[] row = map.get(i);
+            for (int j = 0; j < row.length; j++) {
+                if (row[j] == 'a') {
+                    starts.add(new Location(i, j));
+                }
+            }
+        }
+        int res = floodfill(starts, end);
+        assert res == 443;
+        LOGGER.info("a -> E in {} steps", res);
+    }
+
+    private int floodfill(List<Location> starts, Location end) {
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
         int nRow = map.size();
         int nCol = map.get(0).length;
         int[][] v = new int[nRow][nCol];
         Deque<Location> queue = new ArrayDeque<>();
-        queue.addLast(start);
-        v[start.r][start.c] = 1;
+        for (Location start : starts) {
+            queue.addLast(start);
+            v[start.r][start.c] = 1;
+        }
         while (!queue.isEmpty()) {
             Location current = queue.removeFirst();
             for (int i = 0; i < dr.length; i++) {
@@ -65,44 +90,17 @@ public class HillClimbing {
                     Location next = new Location(nr, nc);
                     queue.addLast(next);
                     if (nr == end.r && nc == end.c) {
-                        assert v[nr][nc] - 1 == 449;
-                        LOGGER.info("S -> E in {} steps", v[nr][nc] - 1);
-                        return;
+                        return v[nr][nc] - 1;
                     }
                 }
             }
         }
+        return -1;
     }
-    public void task2() {
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
-        int nRow = map.size();
-        int nCol = map.get(0).length;
-        int[][] v = new int[nRow][nCol];
-        Deque<Location> queue = new ArrayDeque<>();
-        queue.addLast(end);
-        v[end.r][end.c] = 1;
-        while (!queue.isEmpty()) {
-            Location current = queue.removeFirst();
-            for (int i = 0; i < dr.length; i++) {
-                int nr = current.r + dr[i];
-                int nc = current.c + dc[i];
-                if (0 <= nr && nr < nRow && 0 <= nc && nc < nCol && v[nr][nc] == 0
-                        &&  map.get(current.r)[current.c] - map.get(nr)[nc] <= 1) {
-                    v[nr][nc] = v[current.r][current.c] + 1;
-                    Location next = new Location(nr, nc);
-                    queue.addLast(next);
-                    if (map.get(nr)[nc] == 'a') {
-                        assert v[nr][nc] - 1 == 443;
-                        LOGGER.info("a -> E in {} steps", v[nr][nc] - 1);
-                        return;
-                    }
-                }
-            }
-        }
-    }
+
     static class Location {
         int r, c;
+
         Location(int r, int c) {
             this.r = r;
             this.c = c;
